a9fcf580d64470d51a10251886912250e31a29a0
taurus5650 <taurus_5650@hotmail.com>
2024-04-07 01:45:13 +0800

    options.add_argument(--headless=new)

--- action/driver.py
+++ action/driver.py
@@ -35,7 +35,7 @@ class Driver:
             options = webdriver.ChromeOptions()
             options.add_argument("--verbose")
             options.add_argument("--no-sandbox")
-            options.add_argument("--headless")
+            options.add_argument("--headless=new")
             options.add_argument("--ignore-certificate-errors")
             options.add_argument("--disable-dev-shm-usage")
             options.add_argument("--disable-extensions")
